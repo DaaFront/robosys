4282004d92b6709f91437a52e7fc794822cbd8bf
Daa=Front <hikarumaeda@DESKTOP-AFNQU1T.localdomain>
2022-10-27 12:26:16 +0900

    apdate

--- test.bash
+++ test.bash
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-seq 5 | ./plus
+
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
